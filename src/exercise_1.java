bb57a9f5c1f394581f35d723429e3a280e11c5a0
AnilI2006 <contact.anirudha.hota@gmail.com>
2025-07-25 20:24:36 +0530

    Add files via upload

---
+++ src/exercise_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercise_1 {
+    public static void main(String[] args) {
+//        question 1
+//        int a = 5;
+//        int b = 6;
+//        int c = 8;
+//        int sum = a+b+c;
+//        System.out.println(sum);
+
+//        question 2
+//        float subject1= 45;
+//        float subject2= 95;
+//        float subject3= 48;
+//            float cgpa =(subject1+subject2+subject3/30);
+//        System.out.println(cgpa);
+
+//        question 3
+
+        System.out.println("what is your name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hello " + name +  " have a good day");
+    }
+}
