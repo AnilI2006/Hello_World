bf0a2a2db1a3f7308abcd23aa8d70ee882fa93c2
AnilI2006 <contact.anirudha.hota@gmail.com>
2025-07-25 19:22:58 +0530

    Add files via upload

---
+++ src/Lecture_2.java
@@ -0,0 +1,11 @@
+public class Lecture_2 {
+    public static void main(String[] args) {
+        System.out.print("The sum of these numbers = ");
+        int num1 = 12;
+        int num2 = 23;
+        int num3 = 34;
+        int sum = num1 + num2 + num3 ;
+        System.out.println(sum);
+
+    }
+}
