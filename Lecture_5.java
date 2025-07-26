a351f06fa4f88b3654206255ea6960668acd8dd2
AnilI2006 <contact.anirudha.hota@gmail.com>
2025-07-26 18:52:18 +0530

    Add files via upload

---
+++ Lecture_5.java
@@ -0,0 +1,15 @@
+public class Lecture_5 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6+a;
+        System.out.println(b);
+
+        int c =4;
+        int d = 6*a;
+        System.out.println(d);
+
+        int s = 6;
+        int h = 36/s;
+        System.out.println(h);
+    }
+}
