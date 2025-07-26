6c2380e4f2c7aad3a9667dca8999453971356377
AnilI2006 <contact.anirudha.hota@gmail.com>
2025-07-26 19:19:49 +0530

    Add files via upload

---
+++ src/Lecture_6.java
@@ -0,0 +1,25 @@
+public class Lecture_6 {
+    public static void main(String[] args) {
+//        1.Arithmetic operators
+        int a = 4;
+//        int b = 6 % a;//modulo operator
+//       4.8%1.1 --> returns decimal remainder
+//        2. Assignment operators
+        int b = 9;
+        b *= 3;
+        System.out.println(b);
+//       3.comparision operators
+//        System.out.println(64<6);
+
+//        4. logical operators
+//        System.out.println(64>5 && 64>98);
+        System.out.println(64>5 || 64>98);
+
+//        5. bitwise operators
+        System.out.println(2&3);
+//        10
+//        11
+//  ---------------
+//        10
+    }
+}
