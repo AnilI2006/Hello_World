57b0254358c27ed80e0bbc1db1d90dcd21fb8bd9
AnilI2006 <contact.anirudha.hota@gmail.com>
2025-08-05 21:12:18 +0530

    Add files via upload

---
+++ src/Rock_Paper_Scissor.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Rock_Paper_Scissor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entern 0 for Rock \n 1 for Paper \n 2 for Scissor");
+        int userInput = sc.nextInt();
+
+        Random random = new Random();
+        int botInput = random.nextInt(3);
+
+        if (userInput == botInput){
+            System.out.println("Draw");
+        } else if (userInput == 0 && botInput == 2 || userInput == 1 && botInput == 0
+                || userInput == 2 && botInput == 1){
+            System.out.println("User Win");
+        }else {
+            System.out.println("Bot Win");
+        }
+
+        if (botInput == 0){
+            System.out.println("Bot choice: Rock");
+        } else if (botInput == 1) {
+            System.out.println("Bot choice: Paper");
+        }else if (botInput ==2){
+            System.out.println("Bot choice: Scissor");
+        }
+    }
+}
