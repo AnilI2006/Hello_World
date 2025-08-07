dd537c7f62af660c4102a0afd791caa1abf41ae4
AnilI2006 <contact.anirudha.hota@gmail.com>
2025-08-07 19:56:06 +0530

    Add files via upload

---
+++ src/Lecture_12.java
@@ -0,0 +1,14 @@
+public class Lecture_12 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<=30){
+            System.out.println(i);
+            i++;
+        }
+//       INFINITE LOOP
+//        while (true){
+//            System.out.println("My name is anil");
+//        }
+    }
+
+}
