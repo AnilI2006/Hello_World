b6ef3cc32d8792ceb90a0c43312d92894ddd592d
AnilI2006 <contact.anirudha.hota@gmail.com>
2025-08-13 20:23:39 +0530

    Add files via upload

---
+++ src/Lecture_13.java
@@ -0,0 +1,14 @@
+public class Lecture_13 {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a<=5) {
+            System.out.println(a);
+            a++;
+        }
+        int b = 10;
+        do{
+            System.out.println(b);
+            b++;
+        }while (b<5);
+    }
+}
