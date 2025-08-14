34600d4144882c2773c9d69044ea2532e67ad9c5
AnilI2006 <contact.anirudha.hota@gmail.com>
2025-08-14 21:01:53 +0530

    Add files via upload

---
+++ src/Lecture_14.java
@@ -0,0 +1,17 @@
+public class Lecture_14 {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+//        2n= Even nimbers = 0,2,4,6,8
+//         2n+1= odd numbers = 1,3,5,7,9
+        int n = 5;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(2*i+1);
+        }
+
+        for (int i=7; i>=0; i--){
+            System.out.println(i);
+        }
+    }
+    }
