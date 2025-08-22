a6a46737f89612253c8f69574c9afadea229050f
AnilI2006 <contact.anirudha.hota@gmail.com>
2025-08-22 20:45:18 +0530

    Add files via upload

---
+++ src/Exercise_7.java
@@ -0,0 +1,33 @@
+public class Exercise_7 {
+    public static void main(String[] args) {
+//        problem 1
+//        int n = 7;
+//        for (int i = n; i>0; i--) {
+//            for (int j = 0; j<i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+
+//        problem 2
+//        int sum=0;
+//        int n=4;
+//      for (int i=0; i<n; i++){
+//          sum = sum + (2*i);
+//      }
+//        System.out.print("Sum of even numbers: ");
+//        System.out.println(sum);
+
+//        problem 3
+//        int n = 9;
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.printf("%d X %d = %d\n", n, i, n*i);
+//        }
+
+//        problem 4
+        int n = 9;
+        for (int i = 10; i >= 1; i--) {
+            System.out.printf("%d X %d = %d\n", n, i, n * i);
+        }
+    }
+}
